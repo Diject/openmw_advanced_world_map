1f4a13a8c04ca52a87fb37ff6a04858cb3baf156
Diject <alexkozlovv@gmail.com>
2025-10-29 21:07:38 +0300

    add annotations for event handler registration

--- scripts/advanced_world_map/eventSys.lua
+++ scripts/advanced_world_map/eventSys.lua
@@ -29,6 +29,23 @@ this.handlers = {}
 
 ---@param eventId string
 ---@param handlerFunc fun(e : table)
+---@overload fun(eventId : "onMenuOpened", handlerFunc: fun(e : {menu : advancedWorldMap.ui.menu.map}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMapInitialized", handlerFunc: fun(e : {menu : advancedWorldMap.ui.menu.map, mapWidget : advancedWorldMap.ui.mapWidgetMeta, cellId : string?}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMapShown", handlerFunc: fun(e : {menu : advancedWorldMap.ui.menu.map, mapWidget : advancedWorldMap.ui.mapWidgetMeta, cellId : string?}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMarkerClick", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onMarkerClicked", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMarkerTooltipShow", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta, content : any}) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onMarkerTooltipShowed", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta, content : any, tooltip : any}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMapElementCreated", handlerFunc: fun(e : {mapWidget : advancedWorldMap.ui.mapWidgetMeta, marker : advancedWorldMap.ui.mapElementMeta}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMapElementRemoved", handlerFunc: fun(e : {mapWidget : advancedWorldMap.ui.mapWidgetMeta, marker : advancedWorldMap.ui.mapElementMeta}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onMousePress", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}, offset : any, position : any, button : integer) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onMouseRelease", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}, offset : any, position : any, button : integer) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onFocusLoss", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onMouseMove", handlerFunc: fun(e : {marker : advancedWorldMap.ui.mapElementMeta}, offset : any, position : any) : (boolean?, boolean?), priority : number?)
+---@overload fun(eventId : "onRightMouseMenu", handlerFunc: fun(e : {content : any, relPos : any}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onResized", handlerFunc: fun(e : {menu : advancedWorldMap.ui.menu.map, size : any, mapWidgetSize : any}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onZoomed", handlerFunc: fun(e : {mapWidget : advancedWorldMap.ui.mapWidgetMeta, zoom : number}) : (boolean?), priority : number?)
+---@overload fun(eventId : "onSearch", handlerFunc: fun(e : {results : any[], filter : string, params : any}) : (boolean?), priority : number?)
 function this.registerHandler(eventId, handlerFunc, priority)
     this.handlers[eventId] = this.handlers[eventId] or {}
     this.handlers[eventId][handlerFunc] = {handlerFunc, priority or 0}
