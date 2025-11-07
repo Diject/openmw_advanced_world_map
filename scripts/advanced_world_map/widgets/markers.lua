07fc68ff60e5768f39aa7b640ec53af4dccec8ba
Diject <alexkozlovv@gmail.com>
2025-11-07 21:23:28 +0300

    handle only left mouse button for marker clicks

--- scripts/advanced_world_map/widgets/markers.lua
+++ scripts/advanced_world_map/widgets/markers.lua
@@ -427,7 +427,7 @@ local function createMarkers(widget, cellId)
                 },
                 events = {
                     mouseRelease = function (e, layout, pressed)
-                        if not pressed or not this.activeMenuMeta then return end
+                        if e.button ~= 1 or not pressed or not this.activeMenuMeta then return end
                         if eventSys.triggerEvent(eventSys.events.onMarkerClick, {marker = imageMarkerHandler}) then
                             return
                         end
