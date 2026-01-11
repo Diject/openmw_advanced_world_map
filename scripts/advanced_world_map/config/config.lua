fb42278e681d9a6c3f768a20d680a9b734eb4f1b
Diject <alexkozlovv@gmail.com>
2026-01-11 10:40:05 +0300

    Update config.lua

--- scripts/advanced_world_map/config/config.lua
+++ scripts/advanced_world_map/config/config.lua
@@ -35,7 +35,7 @@ this.default = {
         alpha = {
             region = 8,
             entrance = 80,
-            city = 50,
+            city = 60,
         },
         visibility = {
             regions = true,
