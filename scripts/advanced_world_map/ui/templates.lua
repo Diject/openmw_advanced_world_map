f34ea2441d04e984cf0a9a23033709ce648aed25
Diject <alexkozlovv@gmail.com>
2026-08-17 20:17:47 +0300

    add background template

--- scripts/advanced_world_map/ui/templates.lua
+++ scripts/advanced_world_map/ui/templates.lua
@@ -18,4 +18,51 @@ pcall(function ()
 end)
 
 
+this.roundedBackground = {
+    type = ui.TYPE.Container,
+    content = ui.content{
+        {
+            type = ui.TYPE.Flex,
+            props = {
+                horizontal = true,
+                autoSize = false,
+                relativeSize = util.vector2(1, 1),
+            },
+            content = ui.content {
+                {
+                    type = ui.TYPE.Image,
+                    props = {
+                        resource = ui.texture{ path = "textures/icons/advanced_world_map/rWhiteRect_left.png" },
+                        color = config.data.ui.backgroundColor,
+                        alpha = 0.5,
+                        size = util.vector2(4, 0),
+                        relativeSize = util.vector2(0, 1)
+                    },
+                },
+                {
+                    type = ui.TYPE.Image,
+                    props = {
+                        resource = ui.texture{ path = "textures/icons/advanced_world_map/rWhiteRect_center.png" },
+                        color = config.data.ui.backgroundColor,
+                        alpha = 0.5,
+                        size = util.vector2(-8, 0),
+                        relativeSize = util.vector2(1, 1)
+                    },
+                },
+                {
+                    type = ui.TYPE.Image,
+                    props = {
+                        resource = ui.texture{ path = "textures/icons/advanced_world_map/rWhiteRect_right.png" },
+                        color = config.data.ui.backgroundColor,
+                        alpha = 0.5,
+                        size = util.vector2(4, 0),
+                        relativeSize = util.vector2(0, 1)
+                    },
+                }
+            }
+        },
+    }
+}
+
+
 return this
